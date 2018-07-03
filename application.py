fc7057527d8fa180fa7479e5b8ced39cb377e2cf
jenmo <jenmo316@gmail.com>
2018-07-03 18:10:32 -0400

    first

--- application.py
+++ application.py
@@ -1,10 +1,7 @@
-import datetime
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    now = datetime.datetime.now()
-    fourth = now.month == 7 and now.day == 4
-    return render_template("index.html", fourth=fourth)
+    return "Hello, world!"
